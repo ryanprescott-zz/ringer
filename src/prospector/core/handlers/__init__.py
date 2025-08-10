5ff358a794b33c52dcb703cc67d14d0ef6a46640
Your Name <you@example.com>
2025-08-10 13:25:43 -0600

    Fixed handlers __init__.py file.

--- src/prospector/core/handlers/__init__.py
+++ src/prospector/core/handlers/__init__.py
@@ -1,8 +1,8 @@
 """handlers - Implementation of handlers for Prospector."""
 
 from .crawl_record_handler import CrawlRecordHandler
-from .fs_store_crawl_record_handler import FsStoreCrawlRecordHandler
-from .service_crawl_record_handler import ServiceCrawlRecordHandler
+from .fs_store_handler import FsStoreCrawlRecordHandler
+from .service_call_handler import ServiceCrawlRecordHandler
 
 __version__ = "1.0.0"
 __all__ = [
